afffba80dadf5751bab3bffa8fa9c78601f8ec0f
timqian <timqian92@qq.com>
2018-05-28 15:00:17 +0800

    step 5

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { Prisma } = require('prisma-binding')
 
 let links = [{
     id: 'link-0',
@@ -10,18 +11,19 @@ let idCount = links.length
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
+    feed: (root, args, context, info) => {
+        return context.db.query.links({}, info)
+    },
   },
   Mutation: {
-    post: (root, args) => {
-       const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      }
-      links.push(link)
-      return link
-    }
+    post: (root, args, context, info) => {
+      return context.db.mutation.createLink({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      }, info)
+    },
   },
   Link: {
     id: (root) => root.id,
@@ -34,5 +36,14 @@ const resolvers = {
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'https://eu1.prisma.sh/public-dewpainter-938/hackernews-node/dev',
+      secret: 'mysecret123',
+      debug: true,
+    }),
+  }),
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
